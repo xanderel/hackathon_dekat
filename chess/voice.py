a8a2f49cf5f31e2c60e40490bd1aea411e331011
Knospmoht <100261609+Knospmoht@users.noreply.github.com>
2023-02-25 20:00:28 -0600

    Update voice.py

--- chess/voice.py
+++ chess/voice.py
@@ -1,7 +1,8 @@
 import speech_recognition as sr
 import string
-
+import keyboard
 def record():
+    keyboard.wait('space')
     r = sr.Recognizer()
     with sr.Microphone() as source:
         r.adjust_for_ambient_noise(source)
